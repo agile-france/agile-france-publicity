971a76bb2898295d3076ae51d05057f6fe726ff8
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-05-10 06:57:25 +0200

    Also exporting png 300 DPI

--- scripts/generate-badges-A4.sh
+++ scripts/generate-badges-A4.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
 PDFFILE=../generated/badges-A4.pdf
+PNGFILE=../generated/badges-A4.png
 
 if [ -f $PDFFILE ]; then
 	echo $PDFFILE already exists, deleting...
 	rm $PDFFILE
 fi
 
-inkscape --without-gui --file=../src/badges-A4.svg --export-area-page --export-pdf $PDFFILE
+if [ -f $PNGFILE ]; then
+	echo $PNGFILE already exists, deleting...
+	rm $PNGFILE
+fi
+
+inkscape --without-gui --file=../src/badges-A4.svg --export-dpi=300 --export-area-page --export-pdf $PDFFILE --export-png $PNGFILE
 
 if [ -f $PDFFILE ]; then
 	echo $PDFFILE sucessfully generated.
 else
 	echo Fail to generate $PDFFILE.
+fi
+
+if [ -f $PNGFILE ]; then
+        echo $PNGFILE sucessfully generated.
+else
+        echo Fail to generate $PNGFILE.
 fi
