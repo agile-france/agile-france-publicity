4e26346431ce547d530d61894484a62eac49ba10
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-05-10 07:37:24 +0200

    reduce code duplication

--- scripts/generate-badges-A4.sh
+++ scripts/generate-badges-A4.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 
+deleteExistingFile () {
+	if [ -f $1 ]; then
+        	echo $1 already exists, deleting...
+        	rm $1
+	fi
+}
+
+printExportedFileSuccess () {
+	if [ -f $1 ]; then
+		echo $1 sucessfully generated.
+	else
+		echo Fail to generate $1.
+	fi
+}
+
 PDFFILE=../generated/badges-A4.pdf
 PNGFILE=../generated/badges-A4.png
 
-if [ -f $PDFFILE ]; then
-	echo $PDFFILE already exists, deleting...
-	rm $PDFFILE
-fi
-
-if [ -f $PNGFILE ]; then
-	echo $PNGFILE already exists, deleting...
-	rm $PNGFILE
-fi
+deleteExistingFile $PDFFILE
+deleteExistingFile $PNGFILE
 
 inkscape --without-gui --file=../src/badges-A4.svg --export-dpi=300 --export-area-page --export-pdf $PDFFILE --export-png $PNGFILE
 
-if [ -f $PDFFILE ]; then
-	echo $PDFFILE sucessfully generated.
-else
-	echo Fail to generate $PDFFILE.
-fi
-
-if [ -f $PNGFILE ]; then
-        echo $PNGFILE sucessfully generated.
-else
-        echo Fail to generate $PNGFILE.
-fi
+printExportedFileSuccess $PDFFILE
+printExportedFileSuccess $PNGFILE
