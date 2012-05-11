1adb190f42bd2dfa744d8afb1d2495ac35de502b
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-05-11 08:20:34 +0200

    change resolution from 300 to 200 dpi

--- scripts/generate-flyer.sh
+++ scripts/generate-flyer.sh
@@ -21,7 +21,7 @@ PNGFILE=../generated/flyer-agile-france.png
 deleteExistingFile $PDFFILE
 deleteExistingFile $PNGFILE
 
-inkscape --without-gui --file=../src/flyer/flyer-agile-france.svg --export-dpi=300 --export-area-page --export-pdf $PDFFILE --export-png $PNGFILE
+inkscape --without-gui --file=../src/flyer/flyer-agile-france.svg --export-dpi=150 --export-area-page --export-pdf $PDFFILE --export-png $PNGFILE
 
 printExportedFileSuccess $PDFFILE
 printExportedFileSuccess $PNGFILE
