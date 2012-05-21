682b457b32dd3c67287084d02bc882484a3340ee
Eric Le Merdy <eric.lemerdy@laposte.net>
2012-05-21 07:58:50 +0200

    Export text to path because not everybody has our fonts

--- scripts/generate-flocage.sh
+++ scripts/generate-flocage.sh
@@ -19,6 +19,6 @@ FLOCAGEFILE=../generated/flocage.png
 
 deleteExistingFile $FLOCAGEFILE
 
-inkscape --without-gui --file=../src/flocage.svg --export-dpi=300 --export-area-page --export-png $FLOCAGEFILE
+inkscape --without-gui --file=../src/flocage.svg --export-dpi=300 --export-area-page --export-png $FLOCAGEFILE --export-text-to-path
 
 printExportedFileSuccess $FLOCAGEFILE
--- scripts/generate-plan.sh
+++ scripts/generate-plan.sh
@@ -19,6 +19,6 @@ PLANFILE=../generated/plan.png
 
 deleteExistingFile $PLANFILE
 
-inkscape --without-gui --export-id=export --file=../src/plan.svg --export-dpi=300 --export-png $PLANFILE
+inkscape --without-gui --export-id=export --file=../src/plan.svg --export-dpi=300 --export-png $PLANFILE --export-text-to-path
 
 printExportedFileSuccess $PLANFILE
